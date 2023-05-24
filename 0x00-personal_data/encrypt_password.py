94536a9c5da7f067e069d61bd12e7ca1b1c82172
Paul Okoli <paulokoli7@gmail.com>
2023-05-24 10:29:38 +0000

    5. Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+5. Encrypting passwords
+"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Salted password generation.
+    """
+
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())

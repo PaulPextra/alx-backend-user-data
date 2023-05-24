4e4cae38598c82debcb1ccfe87f035e5ada799f5
Paul Okoli <paulokoli7@gmail.com>
2023-05-24 09:53:07 +0000

    1. Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,3 +18,28 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(rf"{f}=(.*?)\{separator}",
                          f'{f}={redaction}{separator}', message)
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Class properties Initialization
+        """
+
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formater
+        """
+
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)

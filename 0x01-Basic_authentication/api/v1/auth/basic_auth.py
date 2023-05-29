56327681aa9c984fa45970cb8f1a18f85e992c0b
Paul Okoli <paulokoli7@gmail.com>
2023-05-29 15:34:41 +0000

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -20,3 +20,17 @@ class BasicAuth(Auth):
         if not authorization_header or type(authorization_header) != str or not authorization_header.startswith("Basic "):
             return
         return "".join(authorization_header.split(" ")[1:])
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        Method: decode_base64_authorization_header.
+        """
+
+        if not base64_authorization_header or type(base64_authorization_header) != str:
+            return
+        try:
+            b64_bytes = base64_authorization_header.encode('utf-8')
+            res = base64.b64decode(b64_bytes)
+            return res.decode('utf-8')
+        except Exception:
+            return

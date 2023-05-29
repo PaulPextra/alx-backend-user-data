cb63d15848b59584a681b161c49fa413e5cd20bf
Paul Okoli <paulokoli7@gmail.com>
2023-05-29 16:16:31 +0000

    13. Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,18 +16,26 @@ class Auth:
         """
         Public Method: require_auth
         """
+
         if not path or not excluded_paths:
             return True
         path = path + '/' if path[-1] != '/' else path
+        has_wildcard = any(x.endswith("*") for x in excluded_paths)
+        if not has_wildcard:
+            return path not in excluded_paths
         for e in excluded_paths:
             if path == e:
                 return False
+            if e.endswith("*"):
+                if path.startswith(e[:-1]):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
         """
         Public Method: authorization_header
         """
+
         if request:
             return request.headers.get("Authorization")
 
@@ -35,4 +43,5 @@ class Auth:
         """
         Public Method: curent_user
         """
+
         return None

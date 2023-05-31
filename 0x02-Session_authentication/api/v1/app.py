e7dc3253e7c6f78b39102ce6347fefa8a7b285b9
Paul Okoli <paulokoli7@gmail.com>
2023-05-31 11:10:36 +0000

    5. Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -50,7 +50,8 @@ def before():
     """
     if auth:
         paths = ['/api/v1/status/',
-                 '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                 '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                 '/api/v1/auth_session/login/']
         if not auth.require_auth(request.path, paths):
             return
         if (not auth.authorization_header(request) and not auth.session_cookie(request)):

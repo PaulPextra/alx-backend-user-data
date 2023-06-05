300a089fc8c2a389dc7db6ea92cfdd5e895929be
Paul Okoli <paulokoli7@gmail.com>
2023-06-05 19:38:08 +0000

    11. Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Flask class
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -31,6 +31,24 @@ def user() -> str:
         return jsonify({"email": f"{email}", "message": "user created"}), 200
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ POST /sessions
+      Return:
+        - message
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    valid_login = AUTH.valid_login(email, password)
+    if valid_login:
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": f"{email}", "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
